d5dc5d5cc8a588f9121a27064af09cdd911f8f6f
= <vinayasharma08@gmail.com>
2022-05-12 17:29:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './containers/Home' 
+import Login from './components/Login'
+import { UserState } from './context/userContext'
 
-function App() {
+const App = () => {
+  const [user, setuser] = useState({})
+
+  useEffect(() => {
+    setuser(user)
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <BrowserRouter>
+        <UserState>
+    <Routes>
+      <Route path='/*' element={<Home user={user} />}/>
+      <Route path='/Login' element={<Login setuser={setuser} />}/>
+    </Routes>
+    </UserState>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
